d929904c21319c1371cbb8094d55dedd7152f219
Lokee86 <otstructures@gmail.com>
2024-09-16 00:43:38 -0700

    updated main to functionality, working on window creation and layout

--- window.py
+++ window.py
@@ -1,25 +1,31 @@
 import tkinter
-import main
+import functionality
 from screeninfo import get_monitors
 
 root = tkinter.Tk()
 root.title("Creature Creator")
 
 # Get the primary monitor's screen size
-monitors = get_monitors()
-for monitor in monitors:
-    if monitor.is_primary:
-        screen_width = monitor.width
-        screen_height = monitor.height
-        break
+def screen_parameters():
+    monitors = get_monitors()
+    for monitor in monitors:
+        if monitor.is_primary:
+            screen_width = monitor.width
+            screen_height = monitor.height
+            return screen_width, screen_height
+    screen_width = monitors[0].width
+    screen_height = monitors[0].height
+    return screen_width, screen_height
 
 # Set the window size to half the screen's hieght and width, center it on screen, and apply the geometry
-window_width = int(screen_width * 0.5)
-window_height = int(screen_height * 0.5)
-x_position = (screen_width + window_width) // 2
-y_position = (screen_height - window_height) // 2
-root.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
+def create_screen():
+    screen_width, screen_height = screen_parameters()
+    window_width = int(screen_width * 0.5)
+    window_height = int(screen_height * 0.5)
+    x_position = (screen_width - window_width) // 2
+    y_position = (screen_height - window_height) // 2
+    root.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
 
-
-
-root.mainloop()
+if __name__ == "__main__":
+    create_screen()
+    root.mainloop()
